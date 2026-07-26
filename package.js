4b701bcf8194d3070e353103c474045d95c73d6c
iiriruuttu-sudo <iiriruuttu@gmail.com>
2026-07-26 14:18:25 +0200

    Initialize package.json for PaltidxR project

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "paltidxr",
+  "version": "1.0.0",
+  "description": "PaltidxR - Secure Script Hosting Platform",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  },
+  "engines": {
+    "node": ">=16.0.0"
+  }
+}
